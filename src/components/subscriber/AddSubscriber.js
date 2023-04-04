9eeed1197c0cc34df036f8c75fe434bee899a3c1
angadrazz <angadraj9534@gmail.com>
2023-04-04 09:41:13 +0530

    addsubscreiber js

--- src/components/subscriber/AddSubscriber.js
+++ src/components/subscriber/AddSubscriber.js
@@ -1,6 +1,6 @@
-import './AddSubscriber.css';
-
-const AddSubscriber = () => {
+import Container from '../templates/Container';
+import '../subscriber/AddSubscriber.css';
+const AddSubscriber = (props) => {
 
         const onSubmitHandler = (events) => {
                 events.preventDefault();
@@ -9,11 +9,13 @@ const AddSubscriber = () => {
         return (
                 <div>
                         <form onSubmit={onSubmitHandler}>
+                        <Container className="input">
                         <label htmlFor="name">Name</label>
                         <input id="name" type="text"></input>
                         <label htmlFor="pincode">Pincode</label>
                         <input id="pincode" type="number"></input>
                         <button type="submit">Add Subscriber</button>
+                        </Container>
                         </form>
                 </div>
 
